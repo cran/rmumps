8a1faed7e64b1988cf3eda323fd6af67336e69ea
Serguei Sokol <sokol@insa-toulouse.fr>
2016-02-23 09:34:34 +0000

    version 5.0.1.8

--- tests/testthat/test_rmumps.R
+++ tests/testthat/test_rmumps.R
@@ -14,15 +14,43 @@ test_that("first solving", {
   expect_equal(x, 1:n)
 })
 bb=2*b
-xx<-am$solve(bb)
+xx<-solve(am, bb)
+# test basic usage
 test_that("second solving", {
   expect_equal(xx, 2*(1:n))
 })
+
+# test matrix creation from ijv triade
+a=as(a, "dgTMatrix")
+ai=Rmumps$new(a@i, a@j, a@x, ncol(a))
+xi=solve(ai, b)
+test_that("testing a from i,j,v", {
+  expect_equal(xi, 1:n)
+})
+rm(ai)
+
+# test matrix creation from slam::simple_triplet_matrix
+asl=as.simple_triplet_matrix(a)
+ai=Rmumps$new(asl)
+xi=solve(ai, b)
+test_that("testing a from slam::simple_triplet_matrix", {
+  expect_equal(xi, 1:n)
+})
+rm(asl)
+
+# test sparse rhs as slam::simple_triplet_matrix
+asl=as.simple_triplet_matrix(a)
+eye=solve(ai, asl)
+test_that("testing solve() on slam::simple_triplet_matrix", {
+  expect_equal(eye, diag(n), tol=1e-14)
+})
+rm(ai, asl)
 
 # test error signaling on singular matrix
 rm(am)
 a=Matrix(diag(n)); a[1,1]=0; a[1,2]=1
 am=Rmumps$new(a)
 test_that("singular matrix", {
-  expect_error(am$solve(b), "*rmumps: info\\[1\\]=-10*")
+  expect_error(solve(am, b), "*rmumps: info\\[1\\]=-10*")
 })
+rm(a, am)
